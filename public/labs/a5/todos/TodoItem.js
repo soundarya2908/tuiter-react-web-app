fcfadf0b7d090b20c5cf20e8ec7894c28755181f
soundarya <srikanta.s@northeastern.edu>
2022-10-12 11:06:31 -0400

    todo list completed

--- public/labs/a5/todos/TodoItem.js
+++ public/labs/a5/todos/TodoItem.js
@@ -1,6 +1,11 @@
 const TodoItem = (todo) => {
     return(`
-        <li>${todo}</li>
-    `);
+        <li>
+            <input type="checkbox"
+                ${todo.done ? 'checked' : ''}/>
+            ${todo.title}
+            (${todo.status})
+        </li>`
+    );
 }
 export default TodoItem;
