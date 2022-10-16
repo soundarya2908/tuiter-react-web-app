43bccd70a3ef6e4b43c23e7ea9f7d4a26ce2023d
soundarya <srikanta.s@northeastern.edu>
2022-10-16 17:39:51 -0400

    challenge completed-almost

--- public/tuiter/explore/HomeScreen/index.js
+++ public/tuiter/explore/HomeScreen/index.js
@@ -1,26 +1,27 @@
-import NavigationSidebar from "./NavigationSidebar/index.js";
-import ExploreComponent from "./ExploreScreen/ExploreComponent.js";
-import WhoToFollowList from "./WhoToFollowList/WhoToFollowList.js";
+import NavigationSidebar from "../NavigationSidebar/index.js"
+import PostList from "../PostList/index.js";
+import PostSummaryList from "../PostSummaryList/index.js"
 
-function exploreComponent() {
+function HomeScreen() {
     $('#wd-explore').append(`
         <!-- <h2>Explore</h2> -->
         <div class="row mt-2">
         <div class="col-2 col-md-2 col-lg-1 col-xl-2">
         <!-- <h3>NavigationSidebar</h3> -->
-            ${NavigationSidebar('explore')}
+            ${NavigationSidebar('home')}
         </div>
         <div class="col-10 col-lg-7 col-xl-6">
         <!-- <h3>ExploreComponent</h3> -->
-            ${ExploreComponent()}
+            ${PostList()}
         </div>
         <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
         <!--<h3>WhoToFollowList </h3> -->
-            ${WhoToFollowList()}
+            ${PostSummaryList()}
+            
         </div>
         </div>
 
     `);
 }
 
-$(exploreComponent);
+$(HomeScreen);
