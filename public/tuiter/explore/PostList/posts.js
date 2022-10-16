0a3789b609eb8b807f8d006105f13ff6a9d743bc
soundarya <srikanta.s@northeastern.edu>
2022-10-16 18:00:28 -0400

    trying to set deployment

--- public/tuiter/explore/PostList/posts.js
+++ public/tuiter/explore/PostList/posts.js
@@ -1,11 +1,11 @@
 const posts = [
     {
-        profileImage : "../../../images/elonMuskProfile.jpg",
+        profileImage : "../../../../public/images/elonMuskProfile.jpg",
         userName : "Elon Musk",
         handle : "elonmusk",
         time : "23h",
         description : "Amazing show about <a href=\"#\">@inspiration4x</a> mission!",
-        cardImage : "../../../images/inspiration4x.jpg",
+        cardImage : "../../../../public/images/inspiration4x.jpg",
         cardTitle : "Countdown: Inspiration4 Mission to Space | Netflix Official Site",
         cardDescription : "From training to launch to landing, this all-access docuseries rides along with the Inspiration4 crew on the first all-civilian orbital space...",
         cardLink : "https://www.netflix.com/search?q=inspiration&jbv=81441273",
@@ -15,12 +15,12 @@ const posts = [
         likes : "37.5k"
     },
     {
-        profileImage : "../../../images/newYorkProfile.png",
+        profileImage : "../../../../public/images/newYorkProfile.png",
         userName : "New York Post",
         handle : "nypost",
         time : "23h",
         description : "Grimes seen reading Karl Marx following split with world's richest man, Elon Musk <a href=\"#\">trib.al/nx2Gfaq</a>",
-        cardImage : "../../../images/grimes.jpg",
+        cardImage : "../../../../public/images/grimes.jpg",
         cardTitle : "",
         cardDescription : "",
         cardLink : "",
--- public/tuiter/explore/WhoToFollowList/who.js
+++ public/tuiter/explore/WhoToFollowList/who.js
@@ -1,26 +1,26 @@
 export default [
     {   
-      avatarIcon: "../../../../public/images/java.png",
+      avatarIcon: "../../../images/java.png",
       userName: 'Java', 
       handle: 'Java', 
     },
     {   
-      avatarIcon: "../../../../public/images/relativity-space.png",
+      avatarIcon: "../../../images/relativity-space.png",
       userName: 'Relativity Space',
       handle: 'relativityspace', 
     },
     {   
-      avatarIcon: "../../../../public/images/virgin-galactic.png",
+      avatarIcon: "../../../images/virgin-galactic.png",
       userName: 'Virgin Galactic',
       handle: 'virgingalactic', 
     },
     {   
-      avatarIcon: "../../../../public/images/nasa.png",
+      avatarIcon: "../../../images/nasa.png",
       userName: 'NASA', 
       handle: 'NASA', 
     },
     {   
-      avatarIcon: "../../../../public/images/tesla.png",
+      avatarIcon: "../../../images/tesla.png",
       userName: 'Tesla', 
       handle: 'Tesla', 
     }, 
