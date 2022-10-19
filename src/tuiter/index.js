652886c2575e9daf7a4bca77644b0d86379ef393
soundarya <srikanta.s@northeastern.edu>
2022-10-19 16:39:02 -0400

    Tuiter completed

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,15 +1,26 @@
-import Nav from "../nav";
+// import Nav from "../nav";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
-import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
+import ExploreComponent from "./explore";
 
 function Tuiter() {
     return(
         <div>
-            <Nav/>
-            <NavigationSidebar active="home"/>
-            <WhoToFollowList/>
-            <h1>Tuiter</h1>
+            {/* <Nav/> */}
+            
+            <div className="row mt-2">
+                <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                    <NavigationSidebar active="explore"/>
+                </div>
+                <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+                    style={{"position": "relative"}}>
+                    <ExploreComponent/>
+                </div>
+                <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                    <WhoToFollowList/>
+                </div>
+            </div>
+            {/* <h1>Tuiter</h1> */}
         </div>
     );
 }
