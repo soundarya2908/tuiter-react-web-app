5eab415d88a79952d9a4db0372da9ceb881087fa
soundarya <srikanta.s@northeastern.edu>
2022-10-23 21:11:29 -0400

    challenge done

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -9,7 +9,7 @@ const NavigationSidebar = () => {
 
     return (
         <div className="list-group">
-            <a href = "#" className="list-group-item">Tuiter</a>
+            <a href = "#tuiter" className="list-group-item">Tuiter</a>
             <Link to="/tuiter/home" className={`list-group-item ${active === 'home'?'active':''}`}>
                 Home
             </Link>
@@ -19,27 +19,27 @@ const NavigationSidebar = () => {
             <Link to="/" className="list-group-item">
                 Labs
             </Link>
-            <a href = "#" className={`list-group-item
+            <a href = "#tuiter/notifications" className={`list-group-item
                             ${active === 'notifications'?'active':''}`}>
                 Notifications
             </a>
-            <a href = "#" className={`list-group-item
+            <a href = "#tuiter/messages" className={`list-group-item
                             ${active === 'messages'?'active':''}`}>
                 Messages
             </a>
-            <a href = "#" className={`list-group-item
+            <a href = "#tuiter/bookmarks" className={`list-group-item
                             ${active === 'bookmarks'?'active':''}`}>
                 Bookmarks
             </a>
-            <a href = "#" className={`list-group-item
+            <a href = "#tuiter/lists" className={`list-group-item
                             ${active === 'lists'?'active':''}`}>
                 Lists
             </a>
-            <a href = "#" className={`list-group-item
+            <a href = "#tuiter/profile" className={`list-group-item
                             ${active === 'profile'?'active':''}`}>
                 Profile
             </a>
-            <a href = "#" className={`list-group-item
+            <a href = "#tuiter/more" className={`list-group-item
                             ${active === 'more'?'active':''}`}>
                 More
             </a>
