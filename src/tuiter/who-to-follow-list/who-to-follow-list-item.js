d4c63d8c1d284fddf410bb206dd4b5ca4d4e283e
soundarya <srikanta.s@northeastern.edu>
2022-10-23 22:09:40 -0400

    trying to add images

--- src/tuiter/who-to-follow-list/who-to-follow-list-item.js
+++ src/tuiter/who-to-follow-list/who-to-follow-list-item.js
@@ -13,7 +13,7 @@ const WhoToFollowListItem = (
         <li className="list-group-item">
             <div className="row">
             <div className="col-2">
-                <img className="rounded-circle" height={48} src={`${who.avatarIcon}`} alt="" />
+                <img className="rounded-circle" height={48} src={`images/${who.avatarIcon}`} alt="" />
             </div>
             <div className="col-8">
                 <div className="fw-bold">{who.userName}</div>
