8961b47effe66c55a551f7dd116fa52e05dbab92
soundarya <srikanta.s@northeastern.edu>
2022-10-23 21:18:52 -0400

    trying to deploy

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -19,17 +19,17 @@ const ExploreComponent = () => {
         </div>
         <ul className="nav nav-pills mb-2">
         <li className="nav-item">
-            <a className="nav-link active">For You</a>
+            <a href="#tuiter/for-you" className="nav-link active">For You</a>
         </li>
         <li className="nav-item">
-            <a className="nav-link">Trending</a>
+            <a href="#tuiter/trending" className="nav-link">Trending</a>
         </li>
         <li className="nav-item">
-            <a className="nav-link">News</a>
+            <a href="#tuiter/news" className="nav-link">News</a>
         </li>
         </ul>
         <div className="position-relative mb-2">
-        <img src="images/starship.jpg" className="w-100"/>
+        <img src="images/starship.jpg" alt="" className="w-100"/>
         <h1 className="position-absolute wd-nudge-up text-white">
             SpaceX Starship</h1>
         </div>
