91acb07d43d97b468705734780d4b60e9e772bc7
soundarya <srikanta.s@northeastern.edu>
2022-11-20 00:33:13 -0500

    trying to figure out the issue with links

--- src/nav.js
+++ src/nav.js
@@ -4,9 +4,9 @@ function Nav() {
  return (
    <div>
      <Link to="/">Labs</Link> |
-     <Link to="/">Assignment 6</Link> |
+     <Link to="/a6">Assignment 6</Link> |
      <Link to="/a7">Assignment 7</Link> |
-     <Link to="/tuiter">Tuiter</Link>
+     <Link to="/tuiter/*">Tuiter</Link>
    </div>
  );
 }
